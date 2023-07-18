add4d2298850bd1c4de35f3e2860597012bb29cc
Samuele-Benato <samuele.benato@gmail.com>
2023-07-18 15:03:51 +0200

    feat : add password generator

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+// Enter name
+let firstName = prompt("Inserisci il tuo nome");
+
+// Enter the surname
+let surname = prompt("Inserisci il tuo cognome");
+
+// Enter favorite color
+let favColor = prompt("Inserisci il tuo colore preferito");
+
+favColor = favColor + "21";
+
+// Password
+const password = firstName + surname + favColor;
+
+// Password Message
+const passwordMessage = ` 
+<h1> Benvenuto ${firstName}</h1>
+<h3> Questa è la tua password :</h3>
+<p> ${password} </p>`;
+
+document.getElementById("passwordGenerator").innerHTML = passwordMessage;
